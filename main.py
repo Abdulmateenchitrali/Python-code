f6a5d93a0359335f0c178c4d2238383eb7adc0f0
Abdul mateen <50038993+Abdulmateenchitrali@users.noreply.github.com>
2020-12-28 05:31:17 -0800

    Add files via upload
    Write a program to create a table by using the following
    1. By using for loop
    2. input any number for table(ie. 2)
    3. starting point for table (e.g 1)
    4. Ending point for table (e.g 10)

---
+++ main.py
@@ -0,0 +1,9 @@
+#credit me for code please subscribe my channel
+#Youtube channel link
+# https://www.youtube.com/channel/UCLNiYsIhWrXsFPjPEwZH-zg
+
+num = int(input("Enter any number: "))
+
+for i in range(1,11):
+    
+    print(i,"X",num,"=",num*i)
